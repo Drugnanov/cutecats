a7d53dfe90b5a504f5b25f724206cb6329ea40ff
Drugnanov <slama.michal84@gmail.com>
2018-09-27 10:36:36 +0200

    empty project

---
+++ src/test/java/cz/creativeDock/slama/training/search/SearchApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.creativeDock.slama.training.search;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
